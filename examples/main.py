5234f46f53de7128557fa50cf0d14bdd114be0a9
yos1up <yos1up@gmail.com>
2019-04-20 13:32:54 +0900

    improved examples/main.py

--- examples/main.py
+++ examples/main.py
@@ -1,5 +1,9 @@
 import json
 
+# Directory in which this file is located
+# import os
+# this_directory = os.path.dirname(os.path.abspath(__file__))
+
 class APIResource():
     def on_get(self, req, resp):
         params = req.params # GET parameters
--- self_contained_server.py
+++ self_contained_server.py
@@ -1,7 +1,6 @@
 import falcon
 from falcon_multipart.middleware import MultipartMiddleware
 import json
-import numpy as np
 import os, shutil
 import importlib
 import zipfile
